2c00db4183f559d577c8a6021518d14f2dc1a2c2
KiishiAD <84581331+KiishiAD@users.noreply.github.com>
2024-12-03 11:55:30 +0000

    Delete streamline_eda directory

--- streamline_eda/config.py
+++
@@ -1,9 +0,0 @@
-STREAMLIT_CONFIG = {
-    "page_title": "Streamlit EDA App",
-    "layout": "wide",
-    "initial_sidebar_state": "expanded"
-}
-
-ALLOWED_EXTENSIONS = ["csv", "xlsx", "json", "parquet"]
-
-DATA_DIR = "data"
