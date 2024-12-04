c283b9cdfbe00daaf493c8695a885fa9733c5be6
KiishiAD <84581331+KiishiAD@users.noreply.github.com>
2024-12-04 17:12:37 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+STREAMLIT_CONFIG = {
+  "page_title" : "Streamlit EDA APP",
+  "layout" : "wide",
+  "initial_sidebar_state" : "expanded"
+}
+
+ALLOWED_EXTENSIONS = ["csv", "xlsx","json", "parquet"]
+
+DATA_DIR = "data"
